fa1eb45f6f09de50d8c7a3c643ed9c67afed4bb4
SandaHajaina <hajainasanda@gmail.com>
2023-07-27 20:16:58 +0300

    fix logo footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,7 +35,7 @@ export default function Footer() {
             <Container maxWidth="xlg">
                 <Grid container spacing={5}>
                     <Grid item xs={12} md={4}>
-                        <Box>
+                        <Box sx={{ display: 'block' }}>
                             {/* Ici, vous pouvez insérer votre logo à la place du texte "About Us" */}
                             <img className={classes.logo} src="logo.png" alt="Logo" />
                         </Box>
